36cc57d39a2a657a3c0780a290ead965811f3486
justinbye <jbye.linkedin@gmail.com>
2016-12-08 15:21:24 +0000

    x

--- orgs/models.py
+++ orgs/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 class orgs(models.Model):
     org_name = models.CharField(max_length=150)
-    org_address_line_1 = models.CharField(max_length=100)
-    org_address_line_2 = models.CharField(max_length=100)
-    org_city = models.CharField(max_length=50)
-    org_country = models.CharField(max_length=50)
-    org_website = models.URLField
-    org_phone_fixed = models.CharField(max_length=25)
-    org_phone_mobile = models.CharField(max_length=25)
+    org_address_line_1 = models.CharField(max_length=100, null=True)
+    org_address_line_2 = models.CharField(max_length=100, null=True)
+    org_city = models.CharField(max_length=50, null=True)
+    org_country = models.CharField(max_length=50, null=True)
+    org_website = models.URLField(null=True)
+    org_phone_fixed = models.CharField(max_length=25, null=True)
+    org_phone_mobile = models.CharField(max_length=25, null=True)
 
     def __str__(self):
         return 'Organisation: {}'.format(self.org_name)
