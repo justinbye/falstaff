3cd677a3930438510dca8b2bc85bd5e1bc9bc5c4
justinbye <jbye.linkedin@gmail.com>
2016-12-13 19:27:32 +0000

    Added a whole bunch of other stuff

--- directory/views.py
+++ directory/views.py
@@ -1,7 +1,4 @@
-from django.shortcuts import import get_object_or_404, render
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-from django.views import generic
+from django.shortcuts import HttpResponse
 
 def index(request):
     return HttpResponse("Hello world - directory index!")
